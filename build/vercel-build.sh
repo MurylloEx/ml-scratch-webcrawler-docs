6a5dbf5fa87bbe43d5b91c42100f91af4370eb7c
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 18:34:28 -0300

    chore: force deployment trigger

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Define o nome do usuário e a senha
-USERNAME="c4builder"
-PASSWORD="c4builder"
-
-# Cria o novo usuário e define a senha
-useradd -m $USERNAME
+# Define o nome do usuário
+USERNAME="nobody"
 
 # Define o caminho do diretório do projeto npm
 PROJECT_DIR="$PWD"
@@ -14,5 +10,5 @@ PROJECT_DIR="$PWD"
 chown -R $USERNAME:$USERNAME $PROJECT_DIR
 
 # Executa o script npm com o novo usuário
-su $USERNAME -c "cd $PROJECT_DIR && npm run build --user=$USERNAME"
+sudo su $USERNAME -c "cd $PROJECT_DIR && npm run build --user=$USERNAME"
 
