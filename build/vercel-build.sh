67fbdb7597d57043231541f52d6ee4bc41326d99
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 20:58:19 -0300

    chore: force deployment trigger

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-whoami
+cd .cache/puppeteer/chrome
 
-cd .cache
-ls -la
-
-ls -R puppeteer
-
-cd ../
-
-#!/bin/bash
-mv ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome" ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome_original"
-echo "#!/bin/bash" > ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
-echo "exec \$(dirname \"\$0\")/chrome_original --no-sandbox \"\$@\"" >> ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
-chmod +x ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
-ln -sf "\$(dirname \"\$0\")/.cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome" "/usr/local/bin/chromium"
+mv "linux-1108766/chrome-linux/chrome" "linux-1108766/chrome-linux/chrome_original"
+echo "#!/bin/bash" > "linux-1108766/chrome-linux/chrome"
+echo "exec \$(dirname \"\$0\")/chrome_original --no-sandbox \"\$@\"" >> "linux-1108766/chrome-linux/chrome"
+chmod +x "linux-1108766/chrome-linux/chrome"
+ln -sf "\$(dirname \"\$0\")/linux-1108766/chrome-linux/chrome" "/usr/local/bin/chromium"
 
 npm run build
