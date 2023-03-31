32e71fc944810ea2f12856123b2142bcbce5a84c
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 20:04:36 -0300

    chore: force deployment trigger

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -7,12 +7,5 @@ ls -l
 cd .cache
 ls -l
 
-# Define o nome do usuário
-USERNAME="nobody"
 
-# Define o caminho do diretório do projeto npm
-PROJECT_DIR="$PWD"
-
-# Executa o script npm com o novo usuário
-su $USERNAME -c "cd $PROJECT_DIR && npm run build --user=$USERNAME"
-
+#npm run build
