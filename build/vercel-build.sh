cfd468ac4a886f768e21d7b31678b83fa80cfcfe
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 20:50:19 -0300

    chore: force deployment trigger

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -7,5 +7,11 @@ ls -la
 
 ls -R puppeteer
 
+#!/bin/bash
+mv ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome" ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome_original"
+echo "#!/bin/bash" > ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
+echo "exec \$(dirname \"\$0\")/chrome_original --no-sandbox \"\$@\"" >> ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
+chmod +x ".cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome"
+ln -sf "\$(dirname \"\$0\")/.cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome" "/usr/local/bin/chromium"
 
-#npm run build
+npm run build
