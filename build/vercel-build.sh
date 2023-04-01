750099c3b180fc2d7f612e9faa60e84b649333de
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 21:35:03 -0300

    chore: removed unused files

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -3,9 +3,10 @@
 cd .cache/puppeteer/chrome/linux-*/chrome-linux
 
 mv "chrome" "chrome_original"
+
 echo "#!/bin/bash" > "chrome"
 echo "exec \$(dirname \"\$0\")/chrome_original --no-sandbox \"\$@\"" >> "chrome"
+
 chmod +x "chrome"
-ln -sf "\$(dirname \"\$0\")/chrome" "/usr/local/bin/chromium"
 
 npm run build
