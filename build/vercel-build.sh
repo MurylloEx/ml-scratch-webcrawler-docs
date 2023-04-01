60a3ff856d57a0ac97afb855aa5147ca5efc57b5
MurylloEx <muryllopimenta@gmail.com>
2023-03-31 21:04:02 -0300

    chore: force deployment trigger

--- build/vercel-build.sh
+++ build/vercel-build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cd ".cache/puppeteer/chrome/linux-*/chrome-linux"
+cd .cache/puppeteer/chrome/linux-*/chrome-linux
 
 mv "chrome" "chrome_original"
-
 echo "#!/bin/bash" > "chrome"
 echo "exec \$(dirname \"\$0\")/chrome_original --no-sandbox \"\$@\"" >> "chrome"
-
 chmod +x "chrome"
 ln -sf "\$(dirname \"\$0\")/chrome" "/usr/local/bin/chromium"
 
